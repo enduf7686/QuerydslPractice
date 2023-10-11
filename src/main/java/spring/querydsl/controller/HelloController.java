79f29bfab1fd110f2742f4ad0367b5c3e278e1ba
enduf7686 <enduf768640@gmail.com>
2023-10-11 15:35:03 +0900

    Querydsl 페이징 실험

--- src/main/java/spring/querydsl/controller/HelloController.java
+++ src/main/java/spring/querydsl/controller/HelloController.java
@@ -1,15 +1,29 @@
 package spring.querydsl.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import spring.querydsl.entity.Member;
+import spring.querydsl.repository.MemberRepository;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class HelloController {
 
-    @GetMapping("/hello")
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/save")
     @ResponseBody
-    public String hello() {
-        return "hello";
+    public Page<Member> save(@RequestParam String name, @RequestParam int age, Pageable pageable) {
+        memberRepository.save(new Member(name, age, null));
+
+        return memberRepository.findAll(pageable);
     }
 }
